142c8cec44e9c51b086acaf2da49fa5f9d5d536d
Benoit Pasquier <pasquieb@uci.edu>
2019-04-10 19:04:25 +1000

    style - fix some alignment and some names

--- src/FormulaOneMethod.jl
+++ src/FormulaOneMethod.jl
@@ -3,68 +3,67 @@ module FormulaOneMethod
 using LinearAlgebra
 using DualNumbers, HyperDualNumbers, DiffEqBase
 
-mutable struct Mem
-    s   # s(p)
-    A   # factors of ∇ₓF(s, p)
-    ∇s  # ∇s(p)
-    ∇ₓf # ∇ₓf(s, p)
-    p   # p
+mutable struct Mem # Storage for efficient reuse
+    s     # s(p)
+    A     # factors of ∇ₓF(s, p)
+    ∇s    # ∇s(p)
+    ∇ₓf   # ∇ₓf(s, p)
+    p     # p
 end
 
 function update_mem!(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)
-    if p ≠ mem.p       # only update if p has changed
+    if p ≠ mem.p                        # only update if p has changed
         update_solution!(F, ∇ₓF, mem, p, alg; options...)
         s, m = mem.s.u, length(p)
         ∇ₚF = hcat([𝔇(F(s, p + ε * e(j,m))) for j in 1:m]...) # Eq.(?)
-        mem.A = factorize(∇ₓF(s,p))  # update factors of ∇ₓF(s, p)
-        mem.∇s .= mem.A \ -∇ₚF    # update ∇s via Eq.(?)
-        mem.∇ₓf .= ∇ₓf(s,p)          # update ∇ₓf(s, p)
-        mem.p = p      # update p
+        mem.A = factorize(∇ₓF(s,p))   # update factors of ∇ₓF(s, p)
+        mem.∇s .= mem.A \ -∇ₚF        # update ∇s               Eq.(?)
+        mem.∇ₓf .= ∇ₓf(s,p)           # update ∇ₓf(s, p)
+        mem.p = p                     # update p
     end
 end
 
 function update_solution!(F, ∇ₓF, mem, p, alg; options...)
     if ~(mem.s isa SteadyStateSolution) || p ≠ mem.s.prob.p
         mem.s isa SteadyStateSolution ? x = mem.s.u : x = mem.s
-        prob = SteadyStateProblem(F, ∇ₓF, x, p) # define problem
-        mem.s = solve(prob, alg; options...) # update s (inner solver)
+        prob = SteadyStateProblem(F, ∇ₓF, x, p)       # define problem
+        mem.s = solve(prob, alg; options...)          # update s
     end
 end
 
-function f̂(f, F, ∇ₓF, mem, p, alg; options...) # objective
+function f̂(f, F, ∇ₓF, mem, p, alg; options...)             # objective
     update_solution_only!(F, ∇ₓF, mem, p, alg; options...)
     return f(mem.s,p)
 end
 
-function ∇f̂(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...) # gradient
+function ∇f̂(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)        # gradient
     update_mem!(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)
     s, ∇s, m = mem.s, mem.∇s, length(p)
-    ∇ₚf = [𝔇(f(s,p + ε * e(j,m))) for j in 1:m]' # Eq. (?)
-    return mem.∇ₓf * ∇s + ∇ₚf               # Eq. (?)
+    ∇ₚf = [𝔇(f(s,p + ε * e(j,m))) for j in 1:m]'    # Eq.(?)
+    return mem.∇ₓf * ∇s + ∇ₚf                       # Eq.(?)
 end
 
-function ∇²f̂(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...) # Hessian
+function ∇²f̂(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)        # Hessian
     update_mem!(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)
     s, A, ∇s, m = mem.s, mem.A, mem.∇s, length(p)
-    A⁻ᵀ∇ₓfᵀ = vec(A' \ mem.∇ₓf') # independent of (j,k)
-    out = zeros(m,m)       # preallocate
-    for j in 1:m, k in j:m # Loop for Eq.(?)
-        pⱼₖ = p + ε₁ * e(j,m) + ε₂ * e(k,m)           # Hyperdual p
-        xⱼₖ = s + ε₁ * ∇s * e(j,m) + ε₂ * ∇s * e(k,m) # Hyperdual x
-        out[j,k] = ℌ(f(xⱼₖ,pⱼₖ)) - ℌ(F(xⱼₖ,pⱼₖ))' * A⁻ᵀ∇ₓfᵀ # Eq.(?)
-        j ≠ k ? out[k,j] = out[j,k] : nothing   # symmetry
+    A⁻ᵀ∇ₓfᵀ = vec(A' \ mem.∇ₓf') # independent of (j,k)!
+    H = zeros(m,m)               # preallocate Hessian matrix
+    for j in 1:m, k in j:m       # loop upper triangle (symmetry)
+        pⱼₖ = p + ε₁ * e(j,m) + ε₂ * e(k,m)              # hyperdual p
+        xⱼₖ = s + ε₁ * ∇s * e(j,m) + ε₂ * ∇s * e(k,m)    # hyperdual x
+        H[j,k] = ℌ(f(xⱼₖ,pⱼₖ)) - ℌ(F(xⱼₖ,pⱼₖ))' * A⁻ᵀ∇ₓfᵀ     # Eq.(?)
+        j ≠ k ? H[k,j] = H[j,k] : nothing
     end
-    return out
+    return H
 end
 
-function initialize_mem(x, p)
+function initialize_mem(x, p)             # function to initialize mem
     n, m = length(x), length(p)
     return Mem(copy(x), nothing, zeros(n,m), zeros(1,n), nothing)
 end
 
-# Helper functions
 e(j, m) = [i == j for i in 1:m]      # j-th basis vector
 𝔇(x) = DualNumbers.dualpart.(x)      # dual part
 ℌ(x) = HyperDualNumbers.ε₁ε₂part.(x) # hyperdual part
 
-end # module
+end
