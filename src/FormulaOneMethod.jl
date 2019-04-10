37d8b3162351059171494e8d5d2e677da749e72b
Benoit Pasquier <pasquieb@uci.edu>
2019-04-11 09:01:05 +1000

    style - italicized mathh in comments

--- src/FormulaOneMethod.jl
+++ src/FormulaOneMethod.jl
@@ -1,25 +1,24 @@
 module FormulaOneMethod
 
-using LinearAlgebra
-using DualNumbers, HyperDualNumbers, DiffEqBase
+using LinearAlgebra, DualNumbers, HyperDualNumbers, DiffEqBase
 
 mutable struct Mem # Storage for efficient reuse
-    s     # s(p)
-    A     # factors of ∇ₓF(s, p)
-    ∇s    # ∇s(p)
-    ∇ₓf   # ∇ₓf(s, p)
-    p     # p
+    s     # 𝑠(𝑝)
+    A     # factors of A = ∇ₓ𝐹(𝑠,𝑝)
+    ∇s    # ∇𝑠(𝑝)
+    ∇ₓf   # ∇ₓ𝑓(𝑠,𝑝)
+    p     # 𝑝
 end
 
 function update_mem!(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)
-    if p ≠ mem.p                        # only update if p has changed
+    if p ≠ mem.p                    # only update mem if 𝑝 has changed
         update_solution!(F, ∇ₓF, mem, p, alg; options...)
         s, m = mem.s.u, length(p)
         ∇ₚF = hcat([𝔇(F(s, p + ε * e(j,m))) for j in 1:m]...) # Eq.(?)
-        mem.A = factorize(∇ₓF(s,p))   # update factors of ∇ₓF(s, p)
-        mem.∇s .= mem.A \ -∇ₚF        # update ∇s               Eq.(?)
-        mem.∇ₓf .= ∇ₓf(s,p)           # update ∇ₓf(s, p)
-        mem.p = p                     # update p
+        mem.A = factorize(∇ₓF(s,p))   # update factors of ∇ₓ𝐹(𝑠,𝑝)
+        mem.∇s .= mem.A \ -∇ₚF        # update ∇𝑠               Eq.(?)
+        mem.∇ₓf .= ∇ₓf(s,p)           # update ∇ₓ𝑓(𝑠,𝑝)
+        mem.p = p                     # update 𝑝
     end
 end
 
@@ -27,7 +26,7 @@ function update_solution!(F, ∇ₓF, mem, p, alg; options...)
     if ~(mem.s isa SteadyStateSolution) || p ≠ mem.s.prob.p
         mem.s isa SteadyStateSolution ? x = mem.s.u : x = mem.s
         prob = SteadyStateProblem(F, ∇ₓF, x, p)       # define problem
-        mem.s = solve(prob, alg; options...)          # update s
+        mem.s = solve(prob, alg; options...)          # update 𝑠
     end
 end
 
@@ -46,11 +45,11 @@ end
 function ∇²f̂(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)        # Hessian
     update_mem!(f, F, ∇ₓf, ∇ₓF, mem, p, alg; options...)
     s, A, ∇s, m = mem.s, mem.A, mem.∇s, length(p)
-    A⁻ᵀ∇ₓfᵀ = vec(A' \ mem.∇ₓf') # independent of (j,k)!
+    A⁻ᵀ∇ₓfᵀ = vec(A' \ mem.∇ₓf') # independent of (𝑗,𝑘)
     H = zeros(m,m)               # preallocate Hessian matrix
     for j in 1:m, k in j:m       # loop upper triangle (symmetry)
-        pⱼₖ = p + ε₁ * e(j,m) + ε₂ * e(k,m)              # hyperdual p
-        xⱼₖ = s + ε₁ * ∇s * e(j,m) + ε₂ * ∇s * e(k,m)    # hyperdual x
+        pⱼₖ = p + ε₁ * e(j,m) + ε₂ * e(k,m)              # hyperdual 𝑝
+        xⱼₖ = s + ε₁ * ∇s * e(j,m) + ε₂ * ∇s * e(k,m)    # hyperdual 𝑥
         H[j,k] = ℌ(f(xⱼₖ,pⱼₖ)) - ℌ(F(xⱼₖ,pⱼₖ))' * A⁻ᵀ∇ₓfᵀ     # Eq.(?)
         j ≠ k ? H[k,j] = H[j,k] : nothing
     end
@@ -62,7 +61,7 @@ function initialize_mem(x, p)             # function to initialize mem
     return Mem(copy(x), nothing, zeros(n,m), zeros(1,n), nothing)
 end
 
-e(j, m) = [i == j for i in 1:m]      # j-th basis vector
+e(j, m) = [i == j for i in 1:m]      # 𝑗ᵗʰ basis vector of ℝᵐ
 𝔇(x) = DualNumbers.dualpart.(x)      # dual part
 ℌ(x) = HyperDualNumbers.ε₁ε₂part.(x) # hyperdual part
 
